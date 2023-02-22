90bcc2833944dee1df66d040a60977b94091f494
Patricia P <andrograf31@gmail.com>
2023-02-22 22:50:34 +0100

    feat: add existing project to repository

---
+++ AutoComplete/TrieNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace AutoComplete
+{
+    public class TrieNode
+    {
+        public char Value { get; set; }
+        public bool IsEndOfWord { get; set; }
+        public Dictionary<char, TrieNode> Children { get; set; }
+
+        public TrieNode(char value)
+        {
+            Value = value;
+            IsEndOfWord = false;
+            Children = new Dictionary<char, TrieNode>();
+        }
+    }
+}
